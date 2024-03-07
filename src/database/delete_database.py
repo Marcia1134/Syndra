b0ca58ca6db8ced2b0c665c477fe0ccff1a2ae65
Marcia1134 <mossmarcia1134@gmail.com>
2024-03-07 10:31:14 +0800

    Refactor code to improve performance and readability

--- src/database/delete_database.py
+++ src/database/delete_database.py
@@ -7,7 +7,20 @@ def delete_database() -> None:
         try:
             os.remove('syndra.db')
         except FileNotFoundError:
+            print("File not found... Continuing anyway...")
             pass
         except Exception as e:
             print(f"An error occurred: {e}\n\nContinuing anyway...")
             pass
+
+    debug_enabled = True
+
+    # DEBUG
+    if debug_enabled == False:
+        return
+
+    if os.path.exists('syndra.db'):
+        print("DB still present... Calling error and stopping code.")
+        exit
+    else:
+        print("All seems good, continuing...")
--- src/load_scripts/load_groups.py
+++ src/load_scripts/load_groups.py
@@ -5,7 +5,7 @@ async def main(bot : Bot, reload : bool = False) -> None:
     if bot.verbose:
         print('Loading groups...')
     ignore = ['load_groups.py', '__init__.py', '__pycache__'] # Files to ignore
-    folder_path = '/home/marcia/Documents/GitHub/Syndra/src/groups'
+    folder_path = '/home/marcie/Documents/GitHub/Syndra/src/groups'
 
     files = os.listdir(folder_path) # Get all files in the folder
 
