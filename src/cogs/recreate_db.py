5138577ccacd753c7d237c4cb1be17110669344f
Marcia1134 <mossmarcia1134@gmail.com>
2024-03-07 11:35:24 +0800

    Add check_db() function call after recreating DB

--- src/cogs/recreate_db.py
+++ src/cogs/recreate_db.py
@@ -11,6 +11,7 @@ class ReDBCommand(commands.Cog):
     async def redb(self, interaction : Interaction) -> None:
         await interaction.response.send_message("Deleteing all tables and recreating DB", ephemeral=True)
         database.recreate_db()
+        check_db()
         print(f"Database Recreated")
     
 async def setup(bot: commands.Bot) -> None:
