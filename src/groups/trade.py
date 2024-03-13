2c8f96942e401ded5387b6a6fc5f8243ae622a58
Marcia1134 <mossmarcia1134@gmail.com>
2024-03-13 23:54:48 +0800

    Remove unused files and update trade command

--- src/groups/trade.py
+++ src/groups/trade.py
@@ -3,7 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
-import trade_app_data as trade
+from database.tables import Wallet, Product, Mail, Transaction, Currency, Server
 
 load_dotenv('config.env')
 
@@ -14,12 +14,69 @@ if getenv("DEBUG") == "True":
 class TradeCommandGroup(app_commands.Group):
     
     @app_commands.command(name="make", description="Create a trade request.")
+    async def trade_request(self, interaction: discord.Interaction, reciever: discord.User, amount : int):
+        
+        # Fetch currency 
+        try:
+            currency = Currency.get(Currency.server == interaction.guild.id)
+        except:
+            await interaction.response.send_message("Currency not set.")
+            return
+
+        print("pass 1")
+
+        # check if the currency exists
+        try:
+            Currency.get(Currency.id == currency.id)
+        except:
+            await interaction.response.send_message(f"Currency {currency} doesn't exist.")
+            return
+        
+        print("pass 2")
+
+        # fetch interactor's wallet
+        try:
+            wallet = Wallet.get(Wallet.id == interaction.user.id, Wallet.currency_id == currency)
+        except:
+            await interaction.response.send_message("You don't have a wallet.")
+            return
+        
+        print("pass 3")
+
+        # fetch reciever's wallet
+        try:
+            reciever_wallet = Wallet.get(Wallet.id == reciever.id, Wallet.currency_id == currency)
+        except:
+            await interaction.response.send_message(f"{reciever.mention} doesn't have a wallet.")
+            return
+        
+        print("pass 4")
+
+        # check if the interactor has enough money
+        if wallet.balance < amount:
+            await interaction.response.send_message("You don't have enough money.")
+            return
+        
+        print("pass 5")
+
+        # complete the trade
+        wallet.balance -= amount
+        reciever_wallet.balance += amount
+        wallet.save()
+        reciever_wallet.save()
+
+        print('pass 6')
+
+        # send verifcation (+ ping both parties)
+        await interaction.response.send_message(f"Trade completed. {reciever.mention} has recieved {amount} {currency.name}.")
+
+    '''@app_commands.command(name="make", description="Create a trade request.")
     async def trade_request(self, interaction: discord.Interaction, reciever: discord.User, product_id: int):
         await trade.MTR(bot=self.bot, 
                   interaction=interaction, 
                   sender=interaction.author.id, 
                   reciever=reciever, 
-                  product_id=product_id)
+                  product_id=product_id)'''
     
 '''    @app_commands.command(name="view", description="List all of the requests made to you.")
     async def trade_view(self, interaction: discord.Interaction):
